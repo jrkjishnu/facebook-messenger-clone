9bf779b47bf5a1f6c70af16a7d3c56852e75e322
Jishnu <jishnurajk4@gmail.com>
2021-05-12 16:06:27 +0530

    color changng message done for each user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, InputLabel, Input, FormControl } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessage] = useState([
+    {username:'kalasu',text:'hlooo'},
+    {username:'jayasu',text:'hi kutta'},
+
+  ]);
+  const [userName,setUserName] = useState('');
+
+  useEffect(()=>
+  {
+    setUserName(prompt('Enter Username'))
+  },[])
+  
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessage([...messages, {username:userName,text:input}]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>facebook messenger clone</h1>
+      <h4>Welcome {userName}</h4>
+      <form>
+        <FormControl>
+          <InputLabel>Enter the Message...</InputLabel>
+          <Input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            aria-describedby="my-helper-text"
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="secondary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+
+      {/* Display the messages */}
+      {
+      messages.map((message) => {
+        return <Message userName={userName} message={message} />
+      })
+      }
     </div>
   );
 }
